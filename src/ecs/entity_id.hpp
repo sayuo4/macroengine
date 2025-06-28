8c293ba0e649f9ff6bdc47ae0e73aacdbcf50ffe
sayuo4 <sayuo7898@gmail.com>
2025-06-29 02:09:39 +0300

    Define EntityID as std::size_t

---
+++ src/ecs/entity_id.hpp
@@ -0,0 +1,13 @@
+// === Entity ID ===
+// Defines the type used to represent entity identifiers in the ECS system.
+
+#ifndef ENTITY_ID_HPP
+#define ENTITY_ID_HPP
+
+#include <cstddef>
+
+namespace macroengine {
+    using EntityID = std::size_t;
+}
+
+#endif
